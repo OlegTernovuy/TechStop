adf781c2c9dec52aaebba1bfee3f3483dfcf26cf
kateryna-len <lenkaterynna057@gmail.com>
2024-01-16 15:27:11 +0200

    fixed comments in repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
           className="object-cover max-h-[360px]"
         />
       </div>
-      <div>test</div>
       <section className="px-4">
         <Link href="/?catalog=true" className="md:hidden">
           <MainButton />
