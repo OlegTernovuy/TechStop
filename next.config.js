53e1e5eb2f5a2f19968f067747f772ded39fd3c6
Mike Gusev <ternovuyo079@gmail.com>
2024-09-19 19:14:45 +0300

    change base url

--- next.config.js
+++ next.config.js
@@ -8,6 +8,6 @@ module.exports = {
     domains: ["dewisebucket.s3.eu-north-1.amazonaws.com"],
   },
   env: {
-    NEXT_PUBLIC_BASE_URL: "https://team-project-server-41ev.onrender.com/api",
+    NEXT_PUBLIC_BASE_URL: "https://tech-stop-hrhe.onrender.com/api",
   },
 };
